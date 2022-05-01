e6c5dd180ceaa710c800f3b5877d29db883ba302
Kacper <kacper_koniarz@wp.pl>
2022-05-01 14:58:07 +0200

    Sudoku implementation V1 with unit tests, added .idea/ to gitignore

--- src/Model/Sudoku.java
+++ src/Model/Sudoku.java
@@ -1,2 +1,177 @@
-package Model;public class Sudoku {
+package Model;
+
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Sudoku {
+
+    static final int SIZE = 9; //Size of the board 9x9 cells
+    private int[][] board; //internal state of the sudoku
+    private final ReentrantLock lock;
+
+    protected Sudoku() {
+        this.board = new int[SIZE][SIZE];
+        lock = new ReentrantLock();
+    }
+
+    //threadsafe getter for board
+    public int[][] getBoard() {
+        lock.lock();
+        try {
+            return board.clone();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static int getSize() {
+        return SIZE;
+    }
+
+    public boolean isCellEmpty(int row, int column) {
+        lock.lock();
+        try {
+            return board[row][column] == 0;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void setEmpty(int row, int column) {
+        lock.lock();
+        try {
+            board[row][column] = 0;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    //try to insert a value into a board return if successful
+    public boolean insert(int row, int column, int value) {
+        lock.lock();
+        try {
+            if(checkIfValid(row, column, value)) {
+                board[row][column] = value;
+                return true;
+            }
+            return false;
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    //Check if a value can be properly inserted into board
+    private boolean checkIfValid(int row, int column, int value) {
+        return isValidInRow(row, value) &&
+                isValidInColumn(column, value) &&
+                isValidInSquare(row, column, value);
+    }
+
+    //checkIfValid with lock for use outside of insert method
+    public boolean isInsertionAllowed(int row, int column, int value) {
+        lock.lock();
+        try {
+            return checkIfValid(row, column, value);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    //Check if a given row doesn't already contain passed value
+    private boolean isValidInRow(int row, int value) {
+        for(int i = 0; i < SIZE; i++) {
+            if(board[row][i] == value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Check if a given column doesn't already contain passed value
+    private boolean isValidInColumn(int column, int value) {
+        for(int i = 0; i < SIZE; i++) {
+            if(board[i][column] == value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Check if a given square doesn't already contain passed value
+    private boolean isValidInSquare(int row, int column, int value) {
+        int cornerX = column - column % 3;
+        int cornerY = row - row % 3;
+        for(int i = cornerY; i < cornerY + 3; i++) {
+            for(int j = cornerX; j < cornerX + 3; j++) {
+                if(board[i][j] == value) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    //Get instance of Sudoku from passed array
+    public static Sudoku fromArray(int[][] array) throws Exception {
+        Sudoku sudoku = new Sudoku();
+
+        //check if passed array is of right size if it's now return empty sudoku
+        if(array.length != SIZE || array[0].length != SIZE) {
+            throw new Exception("Invalid board size! Should be 9x9.");
+        }
+
+        //check if sudoku is valid
+        for(int row = 0; row < SIZE; row++){
+            for(int column = 0; column < SIZE; column++) {
+                //if cell is not empty, and it's current value is not valid
+                int value = array[row][column];
+                if(value != 0 && !sudoku.insert(row,column,value)) {
+                    throw new Exception("Sudoku is not valid! Values repeat neither in columns, rows or squares.");
+                }
+            }
+        }
+        return sudoku;
+    }
+
+    //Get instance of the randomly initialized Sudoku
+    public static Sudoku randomBoard() {
+        Sudoku sudoku = new Sudoku();
+        Random rand = new Random();
+        int numberQuantity = rand.nextInt(15) + 5; //between 5 and 20 numbers will be placed randomly
+        for(int i = 0; i < numberQuantity; i++) {
+            int column = rand.nextInt(9);
+            int row = rand.nextInt(9);
+            int value = rand.nextInt(9);
+            sudoku.insert(row,column,value);
+        }
+        return sudoku;
+    }
+
+    public void printBoard(){
+        lock.lock();
+        try {
+            for(int i = 0; i < SIZE; i++) {
+                for(int j = 0; j < SIZE; j++) {
+                    if(j != 0 && j % 3 ==0) {
+                        System.out.print("| ");
+                    }
+                    System.out.print(board[i][j]+" ");
+                }
+                if(i == 2 || i == 5) {
+                    System.out.print("\n- - - - - - - - - - - ");
+                }
+                System.out.print("\n");
+            }
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    public Sudoku copy() {
+        Sudoku copy = new Sudoku();
+        copy.board = board;
+        return copy;
+    }
 }
--- src/Test/SudokuTest.java
+++ src/Test/SudokuTest.java
@@ -1,4 +1,173 @@
+package Test;
+
+import Model.Sudoku;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SudokuTest {
-  
+    int[][] validBoard;
+    int[][] invalidBoard;
+    int[][] invalidSizeBoard;
+    Sudoku randomSudoku;
+    Sudoku arraySudoku;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        System.out.println("Setup begin\n");
+        validBoard = new int[][] {
+                {1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 3, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0},
+
+                {0, 0, 0, 0, 5, 0, 0, 0, 0},
+                {0, 0, 5, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+                {0, 0, 3, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 3, 0, 0},
+                {0, 0, 0, 0, 0, 5, 0, 0, 0}
+        };
+
+        invalidSizeBoard = new int[10][5];
+
+        invalidBoard = new int[][] {
+                {0, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 2, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 2, 0},
+
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 2, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+        };
+        randomSudoku = Sudoku.randomBoard();
+        try {
+            arraySudoku = Sudoku.fromArray(validBoard);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        assertArrayEquals(validBoard,arraySudoku.getBoard());
+        System.out.println("End of setup\n");
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+        System.out.println("Tear down\n");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBoard() {
+        System.out.println("Get board test\n");
+        int[][] board = randomSudoku.getBoard();
+        assertEquals(Sudoku.getSize(), randomSudoku.getBoard()[0].length);
+        assertEquals(Sudoku.getSize(), randomSudoku.getBoard().length);
+    }
+
+    @org.junit.jupiter.api.Test
+    void isCellEmpty() {
+        System.out.println("Is cell empty test\n");
+        assertFalse(arraySudoku.isCellEmpty(0, 0));
+        assertTrue(arraySudoku.isCellEmpty(1, 1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEmpty() {
+        System.out.println("Set empty test\n");
+        assertFalse(arraySudoku.isCellEmpty(0, 0));
+        arraySudoku.setEmpty(0,0);
+        assertTrue(arraySudoku.isCellEmpty(0, 0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+        System.out.println("Insert test\n");
+        try {
+            Sudoku newSudoku = Sudoku.fromArray(validBoard);
+            assertTrue(newSudoku.isCellEmpty(2, 5)); //cell is empty
+            assertFalse(newSudoku.insert(2, 5, 3)); //invalid placement attempt
+            assertTrue(newSudoku.isCellEmpty(2, 5)); //cell remains empty
+            assertTrue(newSudoku.insert(2, 5, 4)); //valid placement attempt
+            assertFalse(newSudoku.isCellEmpty(2, 5)); //cell no longer empty
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void isInsertionAllowed() {
+        System.out.println("Insertion allowed test\n");
+        assertFalse(arraySudoku.isInsertionAllowed(4, 3, 5));
+        assertTrue(arraySudoku.isInsertionAllowed(4, 3, 6));
+    }
+
+    @org.junit.jupiter.api.Test
+    void fromArray() {
+        System.out.println("From array test\n");
+        Sudoku testSudoku = null;
+        try {
+            testSudoku = Sudoku.fromArray(invalidSizeBoard);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            assertNull(testSudoku);
+        }
+
+        try {
+            testSudoku = Sudoku.fromArray(invalidBoard);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            assertNull(testSudoku);
+        }
+
+        try {
+            testSudoku = Sudoku.fromArray(validBoard);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            assertNotNull(testSudoku);
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void printBoard() {
+        System.out.println("Print board test\n");
+        try {
+            Sudoku printSudoku = Sudoku.fromArray(validBoard);
+            printSudoku.printBoard();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void randomBoard() {
+        System.out.println("Random board test\n");
+        boolean noExceptions = true;
+        for(int i = 0; i < 100; i++) {
+            Sudoku random = Sudoku.randomBoard(); //make random board
+            try {
+                //generate new sudoku using random board to check if it generates valid sudokus
+                Sudoku fromRandom = Sudoku.fromArray(random.getBoard());
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+                noExceptions = false;
+                break;
+            }
+        }
+        assertTrue(noExceptions);
+    }
+
+    @org.junit.jupiter.api.Test
+    void copy() {
+        System.out.println("Copy test\n");
+        Sudoku original = Sudoku.randomBoard();
+        Sudoku copy = original.copy();
+        assertArrayEquals(original.getBoard(),copy.getBoard());
+        assertNotEquals(copy,original);
+    }
 }
